a7ad682e038b707c995f1ddf4f2f2f11132bd501
mirkxamdamov <musyakuu@gmail.com>
2023-04-02 13:48:49 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+let students = [
+    {
+        name: 'Nurilloh',
+        score:95
+    },
+    {
+        name: 'Nurilloh',
+        score:60
+    },
+    {
+        name: 'Nurilloh',
+        score:50
+    },
+    {
+        name: 'Nurilloh',
+        score:40
+    },
+    {
+        name: 'Nurilloh',
+        score:0
+    },
+]
+console.log();
